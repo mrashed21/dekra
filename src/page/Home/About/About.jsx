56363d965d420a88ae0c9c2c2ec3da905f612597
mrashed21 <muhammadrashed760@gmail.com>
2025-03-25 14:34:16 +0600

    [ADD] added about us section

---
+++ src/page/Home/About/About.jsx
@@ -0,0 +1,81 @@
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { motion } from "framer-motion";
+import React, { useEffect } from "react";
+
+const About = () => {
+  useEffect(() => {
+    AOS.init({ duration: 1000 });
+  }, []);
+
+  return (
+    <div className="container mx-auto py-12">
+      <h1
+        className="text-6xl font-extrabold text-primary underline underline-offset-[30px]"
+        data-aos="fade-up"
+      >
+        Why DEKRA?
+      </h1>
+      <div className="flex items-center justify-center mt-20 gap-10">
+        {/* left side */}
+        <div data-aos="fade-right">
+          <motion.div
+            initial={{ opacity: 0, x: -50 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ duration: 0.8 }}
+          >
+            <img
+              src="https://dekra-arbeit.hr/wp-content/uploads/2022/05/Homepage-Usluge.webp"
+              alt="image"
+              className="rounded-4xl"
+            />
+          </motion.div>
+        </div>
+        {/* right side */}
+        <div data-aos="fade-left">
+          <motion.div className="mb-10" data-aos="fade-up">
+            <h1 className="text-3xl font-extrabold text-primary flex">
+              <h1 className="mr-5"> 1.</h1> More than 18 years of experience
+            </h1>
+            <p className="mt-5 pl-14">
+              We have been on the Croatian market since 2003, and over the years
+              we have established ourselves as exceptional experts with
+              specialist knowledge and tools.
+            </p>
+          </motion.div>
+          <motion.div className="mb-10" data-aos="fade-up" data-aos-delay="200">
+            <h1 className="text-3xl font-extrabold text-primary flex">
+              <h1 className="mr-5"> 2.</h1> 7 offices in Croatia
+            </h1>
+            <p className="mt-5 pl-14">
+              In addition to Zagreb, we have offices in Osijek, Slavonski Brod,
+              Pula, Rijeka, Zadar and Split.
+            </p>
+          </motion.div>
+          <motion.div className="mb-10" data-aos="fade-up" data-aos-delay="400">
+            <h1 className="text-3xl font-extrabold text-primary flex">
+              <h1 className="mr-5"> 3.</h1> Reliability, speed and
+              responsibility
+            </h1>
+            <p className="mt-5 pl-14">
+              In DEKRA, we are equally committed to our Client companies and
+              candidates we employ.
+            </p>
+          </motion.div>
+          <motion.div
+            className="flex justify-center"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 1 }}
+          >
+            <button className="bg-primary py-3 px-20 rounded-4xl font-semibold hover:bg-[#71b0a6ef] transition-all ease-in-out">
+              About us
+            </button>
+          </motion.div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
