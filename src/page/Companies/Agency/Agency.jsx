b00a00cdce581c407a5fc0df6de28b0951f3909c
mrashed21 <muhammadrashed760@gmail.com>
2025-03-26 14:01:11 +0600

    [ADD] added agency section

--- src/page/Companies/Agency/Agency.jsx
+++ src/page/Companies/Agency/Agency.jsx
@@ -37,15 +37,10 @@ const Agency = () => {
 
   return (
     <div className="container mx-auto py-16">
-      <h2 className="tracking-wider font-semibold" data-aos="fade-up">
-        INFO CORNER
-      </h2>
-      <hr className="mt-3 border-[#eee] border border-b-2" data-aos="fade-up" />
-
       <div className="flex flex-col lg:flex-row items-center justify-center gap-10 mt-10">
         {/* Left Side */}
         <motion.div
-          className="cursor-pointer overflow-hidden  px-5 lg:px-0 rounded-2xl"
+          className="cursor-pointer overflow-hidden  px-5 lg:px-0 rounded-2xl w-full lg:w-1/3"
           initial={{ opacity: 0, y: 50 }}
           whileInView={{ opacity: 1, y: 0 }}
           viewport={{ once: false }}
@@ -54,62 +49,141 @@ const Agency = () => {
           <img
             src="https://dekra-arbeit.hr/wp-content/uploads/2022/05/Cover-slika-1.webp"
             alt="image"
-            className="w-full h-full object-cover transition-transform duration-300 hover:scale-125 rounded-2xl"
+            className="w-full h-full object-cover transition-transform duration-500 hover:scale-115 rounded-2xl"
           />
         </motion.div>
 
         {/* Right Side */}
+        <div className="w-full lg:w-2/3 ">
+          <div className="">
+            <motion.div
+              initial={{ opacity: 0, y: 50 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              viewport={{ once: false }}
+              transition={{ duration: 0.8, delay: 0.2 }}
+            >
+              <div className="flex items-start gap-4 px-5 lg:px-0">
+                <div>
+                  <h2 className="tracking-widest ml-10 text-xl font-semibold mb-10">
+                    For Agencies
+                  </h2>
+                  <h1 className="text-3xl font-bold text-primary flex items-start gap-4 group">
+                    <span className="text-primary mt-1 text-xl transition-transform duration-300 transform group-hover:translate-x-5">
+                      <BiSolidRightArrow />
+                    </span>
+                    Different options of hiring-out workers under a fixed-term
+                    period.
+                  </h1>
+                  <p className="text-secondary mt-2 ml-10">
+                    Find out why so many renowned companies in the region choose
+                    DEKRA for selecting and providing workers, year after year.
+                  </p>
+                </div>
+              </div>
+            </motion.div>
+          </div>
+          <hr className="mt-48 border-[#eee] border border-b-2" />
+        </div>
+      </div>
+
+      {/* 2nd */}
+      <div className="flex flex-col lg:flex-row items-center justify-center gap-10 mt-10">
+        {/* Left Side */}
         <motion.div
+          className="cursor-pointer overflow-hidden  px-5 lg:px-0 rounded-2xl w-full lg:w-1/3"
           initial={{ opacity: 0, y: 50 }}
           whileInView={{ opacity: 1, y: 0 }}
           viewport={{ once: false }}
-          transition={{ duration: 0.8, delay: 0.2 }}
+          transition={{ duration: 0.8 }}
         >
-          <div className="flex items-start gap-4 px-5 lg:px-0">
-            <div>
-              <h2>For Agencies</h2>
-              <h1 className="text-3xl font-bold text-primary flex items-start gap-4 group">
-                <span className="text-primary mt-1 text-xl transition-transform duration-300 transform group-hover:translate-x-5">
-                  <BiSolidRightArrow />
-                </span>
-                DEKRA is your reliable partner for human resources. 
-              </h1>
-              <p className="text-secondary mt-2 ml-10">
-                Most of us have recently digitized our way of doing business and
-                started new work principles. Online meetings were one of many
-                changes that have occurred in the workplace.
-              </p>
-            </div>
-          </div>
+          <img
+            src="https://dekra-arbeit.hr/wp-content/uploads/2022/05/Cover-HR.webp"
+            alt="image"
+            className="w-full h-full object-cover transition-transform duration-500 hover:scale-115 rounded-2xl"
+          />
         </motion.div>
-      </div>
 
-      <hr
-        className="my-10 border-[#eee] border border-b-2"
-        data-aos="fade-up"
-      />
-
-      <motion.div
-        className="flex items-start gap-4 w-full lg:w-1/3 cursor-pointer px-5 lg:px-0"
-        initial={{ opacity: 0, y: 50 }}
-        whileInView={{ opacity: 1, y: 0 }}
-        viewport={{ once: false }}
-        transition={{ duration: 0.8 }}
-      >
-        <div className="">
-          <h1 className="text-3xl font-bold text-primary flex items-start gap-4 group">
-            <span className="text-primary mt-1 text-xl transition-transform duration-300 transform group-hover:translate-x-5">
-              <BiSolidRightArrow />
-            </span>
-            What is boreout and how to deal with it?
-          </h1>
-          <p className="text-secondary mt-2 ml-10">
-            Although it sounds similar to the term burnout, the boreout still
-            receives much less attention, but it is encountered daily by many
-            employees. You may have faced it too, without even knowing it.
-          </p>
+        {/* Right Side */}
+        <div className=" w-full lg:w-2/3">
+          <div className="">
+            <motion.div
+              initial={{ opacity: 0, y: 50 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              viewport={{ once: false }}
+              transition={{ duration: 0.8, delay: 0.2 }}
+            >
+              <div className="flex items-start gap-4 px-5 lg:px-0">
+                <div>
+                  <h2 className="tracking-widest ml-10 text-xl font-semibold mb-10">
+                    HR CONSULTING
+                  </h2>
+                  <h1 className="text-3xl font-bold text-primary flex items-start gap-4 group">
+                    <span className="text-primary mt-1 text-xl transition-transform duration-300 transform group-hover:translate-x-5">
+                      <BiSolidRightArrow />
+                    </span>
+                    Consultancy services and solutions from the field of human
+                    resources
+                  </h1>
+                  <p className="text-secondary mt-2 ml-10">
+                    From finding and evaluating candidates to researching the
+                    organizational climate and salaries — our HR services cover
+                    everything you need in the field of human resources.
+                  </p>
+                </div>
+              </div>
+            </motion.div>
+          </div>
+          <hr className="mt-48 border-[#eee] border border-b-2" />
         </div>
-      </motion.div>
+      </div>
+      {/* 3rd */}
+      <div className="flex flex-col lg:flex-row items-center justify-center gap-10 mt-10">
+        {/* Left Side */}
+        <motion.div
+          className="cursor-pointer overflow-hidden  px-5 lg:px-0 rounded-2xl w-full lg:w-1/3"
+          initial={{ opacity: 0, y: 50 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          viewport={{ once: false }}
+          transition={{ duration: 0.8 }}
+        >
+          <img
+            src="https://dekra-arbeit.hr/wp-content/uploads/2022/05/Cover-ELS.webp"
+            alt="image"
+            className="w-full h-full object-cover transition-transform duration-500 hover:scale-115 rounded-2xl"
+          />
+        </motion.div>
+
+        {/* Right Side */}
+        <div className=" w-full lg:w-2/3">
+          <div className="">
+            <motion.div
+              initial={{ opacity: 0, y: 50 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              viewport={{ once: false }}
+              transition={{ duration: 0.8, delay: 0.2 }}
+            >
+              <div className="flex items-start gap-4 px-5 lg:px-0">
+                <div>
+                  <h2 className="tracking-widest ml-10 text-xl font-semibold mb-10">
+                    EVENT & LOGISTIC SERVICES
+                  </h2>
+                  <h1 className="text-3xl font-bold text-primary flex items-start gap-4 group">
+                    <span className="text-primary mt-1 text-xl transition-transform duration-300 transform group-hover:translate-x-5">
+                      <BiSolidRightArrow />
+                    </span>
+                    Event management, promotion and mystery shopping.
+                  </h1>
+                  <p className="text-secondary mt-2 ml-10">
+                    Find out about our event management services, promotion
+                    processes or market research.
+                  </p>
+                </div>
+              </div>
+            </motion.div>
+          </div>
+          <hr className="mt-48 border-[#eee] border border-b-2" />
+        </div>
+      </div>
     </div>
   );
 };
---
+++ src/page/Companies/CompanyAbout/CompanyAbout.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CompanyAbout = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default CompanyAbout;
